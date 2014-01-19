7b0b29d9f748c70c7ddf7297cc453a96f9bf91ce
Kevin Xiao <xiaowx2010@gmail.com>
2014-01-20 01:33:45 +0800

    项目修改和延期

--- Controllers/TaskController.cs
+++ Controllers/TaskController.cs
@@ -58,33 +58,19 @@ namespace TODO.Controllers
                 newtask.TaskType = collection["TaskType"];
                 newtask.TaskDeadLine = DateTime.Parse(collection["TaskDeadLine"]);
                 newtask.TaskRemark = collection["TaskRemark"];
+                newtask.TaskStatus = Convert.ToInt32(collection["TaskStatus"]);
 
                 newtask.TaskCreator = "";
 
-                var nodesname = Request.Form.GetValues("NodeRemark").ToList<string>();
-                if (nodesname.Count > 0)
-                {
-                    int num = 1;
-                    foreach (var name in nodesname)
-                    {
-                        if (!string.IsNullOrWhiteSpace(name))
-                        {
-                            TODO_TaskNodes node = new TODO_TaskNodes();
-                            node.NodeName = name;
-                            node.NodeNum = num++;
-                            node.TODO_Tasks = newtask;
-                            newtask.TODO_TaskNodes.Add(node);
-                        }
-                    }
-
-                }
-                var executors = Request.Form.GetValues("TaskExecutor").ToList<string>();
-                if (executors.Count > 0)
+                CreateNodes(newtask);
+                var executors = Request.Form.GetValues("TaskExecutor");
+                if (executors != null && executors.Count() > 0)
                 {
                     foreach (var user in executors)
                     {
                         TODO_Task_User task_user = new TODO_Task_User();
                         task_user.UserID = Convert.ToInt32(user);
+                        task_user.IsAssigned = newtask.TaskStatus==1;
                         newtask.TODO_Task_User.Add(task_user);
 
                     }
@@ -106,7 +92,25 @@ namespace TODO.Controllers
  
         public ActionResult Edit(int id)
         {
-            return View();
+            ViewBag.ActiveType = "TaskControl";
+
+            TaskDataContext db = new TaskDataContext();
+            var task = db.TODO_Tasks.SingleOrDefault<TODO_Tasks>(s => s.ID == id);
+            if (task == null)
+                throw new Exception("该任务不存在！");
+            Init_detail_date(db, task);
+            return View(task);
+        }
+
+        private void Init_detail_date(TaskDataContext db, TODO_Tasks task)
+        {
+            var userlist = from u in db.TODO_Users where u.IsAvailable select u;
+
+            var selectUser = from u in db.TODO_Task_User where u.TaskID == task.ID select u.UserID;
+            var selectList = new MultiSelectList(userlist, "ID", "PersonName", selectUser);
+            var nodelist = from n in db.TODO_TaskNodes where n.TaskID == task.ID select n;
+            ViewData["TaskExecutor"] = selectList;
+            ViewData["nodelist"] = nodelist.ToList<TODO_TaskNodes>();
         }
 
         //
@@ -115,15 +119,93 @@ namespace TODO.Controllers
         [HttpPost]
         public ActionResult Edit(int id, FormCollection collection)
         {
+            ViewBag.ActiveType = "TaskControl";
+
+            TaskDataContext db = new TaskDataContext();
+            var task = db.TODO_Tasks.SingleOrDefault<TODO_Tasks>(s => s.ID == id);
             try
             {
-                // TODO: Add update logic here
- 
+                if (task == null)
+                    throw new Exception("该任务不存在！");
+
+                task.TaskName = collection["TaskName"];
+                task.Priority = Convert.ToInt32(collection["Priority"]);
+                task.TaskType = collection["TaskType"];
+                task.TaskDeadLine = DateTime.Parse(collection["TaskDeadLine"]);
+                task.TaskRemark = collection["TaskRemark"];
+                task.TaskStatus = Convert.ToInt32(collection["TaskStatus"]);
+
+                db.TODO_TaskNodes.DeleteAllOnSubmit(task.TODO_TaskNodes);
+                CreateNodes(task);
+                
+                var now_executors = Request.Form.GetValues("TaskExecutor");
+                if (now_executors != null && now_executors.Count() > 0)
+                {
+                    var all_users = from u in db.TODO_Task_User select u;
+                    var previous_executor_ids = (from u in all_users where u.TaskID == task.ID select u.UserID).ToList<int>();
+                    foreach (var user in now_executors)
+                    {
+                        var uid = Convert.ToInt32(user);
+                        if (!previous_executor_ids.Contains(uid))
+                        {
+                            TODO_Task_User task_user = new TODO_Task_User();
+                            task_user.UserID = uid;
+                            task.TODO_Task_User.Add(task_user);
+                            task_user.IsAssigned = task.TaskStatus == 1;
+                        }
+                        else
+                        {
+                            var modified_user = task.TODO_Task_User.Single(u => u.UserID == uid);
+                            if (modified_user != null)
+                            {
+                                modified_user.IsAssigned = task.TaskStatus == 1;
+                            }
+                        }
+                    }
+                    foreach (var user in previous_executor_ids)
+                    {
+                        if (!now_executors.Contains(user.ToString()))
+                        {
+                            var rm_user = task.TODO_Task_User.Single(u => u.UserID == user);
+                            if (rm_user != null)
+                            {
+                                db.TODO_Task_User.DeleteOnSubmit(rm_user);
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    db.TODO_Task_User.DeleteAllOnSubmit(task.TODO_Task_User);
+                }
+                db.SubmitChanges();
                 return RedirectToAction("Index");
             }
             catch
             {
-                return View();
+                Init_detail_date(db, task);
+                return View(task);
+            }
+        }
+
+        private void CreateNodes(TODO_Tasks task)
+        {
+            var nodesname = Request.Form.GetValues("NodeRemark").ToList<string>();
+            if (nodesname.Count > 0)
+            {
+                int num = 1;
+                foreach (var name in nodesname)
+                {
+                    if (!string.IsNullOrWhiteSpace(name))
+                    {
+                        TODO_TaskNodes node = new TODO_TaskNodes();
+                        node.NodeName = name;
+                        node.NodeNum = num++;
+                        node.TODO_Tasks = task;
+                        task.TODO_TaskNodes.Add(node);
+                    }
+                }
+
             }
         }
 
@@ -145,6 +227,20 @@ namespace TODO.Controllers
                 delmsg = "删除失败！原因：" + ex.ToString();
             }
             return RedirectToAction("Index", new { operateMsg = delmsg });
+
+        }
+
+        [HttpPost]
+        public ActionResult Delay(FormCollection collection)
+        {
+
+            // TODO: Add delete logic here
+            var id = Convert.ToInt32(collection["DelayTask"]);
+            TaskDataContext db = new TaskDataContext();
+            var task = db.TODO_Tasks.SingleOrDefault<TODO_Tasks>(s => s.ID == id);
+            task.TaskDeadLine = Convert.ToDateTime(collection["DelayDate"]);
+            db.SubmitChanges();
+            return RedirectToAction("Index");
 
         }
     }
